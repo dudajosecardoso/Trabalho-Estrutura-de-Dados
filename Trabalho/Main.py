69c83793261af53253fc361085599ca9043a908f
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-04-18 13:16:49 -0300

    Update Main.py

---
+++ Trabalho/Main.py
@@ -0,0 +1,9 @@
+import calculate*
+
+def main():
+    expression= str(input("Digite a Expressão:"))
+    result= calculate(expression)
+    print(f'Resultado: {result}')
+
+if __name__== '__main__':
+    main()
