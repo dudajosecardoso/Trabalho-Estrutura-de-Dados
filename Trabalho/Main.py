6829066fe11a1b93f0636d426da218c9ec79683f
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-04-19 21:19:18 -0300

    Update Main.py

--- Trabalho/Main.py
+++ Trabalho/Main.py
@@ -1,9 +1,30 @@
-import calculate*
+from Calculadora import*
+
+def menu():
+    print("-------------------------")
+    print("\033[0;34mBEM VINDO AO MENU PRINCIPAL\033[m")
+    print("1. Calcular uma expressão")
+    print("2. Sair do sistema")
+    opc = int(input("Digite a ação que deseja realizar: "))
+
+    return opc
 
 def main():
-    expression= str(input("Digite a Expressão:"))
-    result= calculate(expression)
-    print(f'Resultado: {result}')
+    while True:
+        opc = menu()
+        try:
+            if opc == 1:
+                expression= str(input("Digite a Expressão: "))
+                result = calculate(expression)
+                print(f'Resultado: {result}')
+            
+            if opc == 2:
+                print("Obrigado por usar nossa aplicação. Volte sempre!")
+                break
+
+        except ValueError:
+            print("Você não digitou uma opção válida!")
+            return opc
 
 if __name__== '__main__':
     main()
